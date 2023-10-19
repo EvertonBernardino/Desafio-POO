5c3cca47bb6896b1d19b4fe59c7b3f0d8c77ee5c
Everton Bernardino <ceverto11@gmail.com>
2023-10-19 13:11:53 -0300

    Desafio

---
+++ src/Navegador.java
@@ -0,0 +1,12 @@
+public class Navegador {
+    public void exibirPagina(){
+        System.out.println("Pagina WEB");
+    }
+    public void adicionarNovaAba(){
+        System.out.println("Nova Pagina");
+    }
+    public void atualizarPagina(){
+        System.out.println("Atualizando");
+    }
+
+}
---
+++ src/ReprodutorDeMusica.java
@@ -0,0 +1,13 @@
+public class ReprodutorDeMusica {
+    public void tocar(){
+        System.out.println("Tocando");
+    }
+
+    public void pausar(){
+        System.out.println("Em pause");
+    }
+
+    public void selecionarMusica(){
+        System.out.println("Lista de musicas");
+    }
+}
